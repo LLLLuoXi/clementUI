8e78fb138c647f45f5c1bb88301334bd62745e82
luoxi <2652445066@qq.com>
2022-06-01 22:05:04 +0800

    🧪test: 完成Button.spac.js

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -1,41 +1,110 @@
 /*
  * @Author: luoxi
- * @LastEditTime: 2022-05-31 23:48:05
+ * @LastEditTime: 2022-06-01 22:01:29
  * @LastEditors: your name
- * @Description: 
+ * @Description: Button 组件单元测试
  */
 
 import Button from '@clement/Button';
 import { shallowMount } from '@vue/test-utils';
 
 describe('Button.vue', () => {
-  it('create', () => {
-    const wrapper = shallowMount(Button)
-    const buttonEle = wrapper.vm.$el;
+  let wrapper;
+  let buttonEle;
 
+  beforeEach(() => {
+    wrapper = shallowMount(Button);
+    buttonEle = wrapper.vm.$el;
+  })
+
+  //create
+  it('create', () => {
     expect(buttonEle.classList).toContain('cl-button');
   });
 
-  it('size', () => {
-    const wrapper = shallowMount(Button)
-    const buttonEle = wrapper.vm.$el;
-
-    wrapper.setProps({
-      size: "mini"
+  // size
+  it('size', async () => {
+    await wrapper.setProps({
+      type: "mini"
     })
-
     expect(buttonEle.classList).toContain('cl-button--mini');
-    // cl-button--${size}
   })
 
-  it('type', () => {
-    const wrapper = shallowMount(Button)
-    const buttonEle = wrapper.vm.$el;
-
-    wrapper.setProps({
+  // type
+  it('type', async () => {
+    await wrapper.setProps({
       type: "primary"
     })
-
     expect(buttonEle.classList).toContain('cl-button--primary');
+  })
+
+  // plain
+  it('plain', async () => {
+    await wrapper.setProps({
+      plain: true
+    })
+    expect(buttonEle.classList).toContain('is-plain');
+  })
+
+  // round
+  it('round', async () => {
+    await wrapper.setProps({
+      round: true
+    })
+    expect(buttonEle.classList).toContain('is-round');
+  })
+
+  // circle
+  it('circle', async () => {
+    await wrapper.setProps({
+      circle: true
+    })
+    expect(buttonEle.classList).toContain('is-circle');
+  })
+
+  // disabled
+  it('disabled', async () => {
+    await wrapper.setProps({
+      disabled: true
+    })
+    expect(buttonEle.classList).toContain('is-disabled');
+  })
+
+  // loading
+  it('loading', async () => {
+    await wrapper.setProps({
+      loading: true
+    })
+    expect(wrapper.html()).toContain('<i class="cl-icon-loading"></i>');
+  })
+
+  // icon
+  it('icon', async () => {
+    await wrapper.setProps({
+      icon: 'cl-icon-edit'
+    })
+    expect(wrapper.html()).toContain('<i class="cl-icon-edit"></i>');
+  })
+
+  // autofocus
+  it('autofocus', async () => {
+    await wrapper.setProps({
+      autofocus: true
+    })
+    expect(wrapper.attributes().autofocus).toEqual('')
+  })
+
+  // native-type
+  it('native-type', async () => {
+    await wrapper.setProps({
+      nativeType: 'submit'
+    })
+    expect(wrapper.attributes().type).toEqual('submit')
+  })
+
+  // $emit event
+  it('click', async () => {
+    await wrapper.trigger('click')
+    expect(wrapper.emitted().click).toBeTruthy()
   })
 });
